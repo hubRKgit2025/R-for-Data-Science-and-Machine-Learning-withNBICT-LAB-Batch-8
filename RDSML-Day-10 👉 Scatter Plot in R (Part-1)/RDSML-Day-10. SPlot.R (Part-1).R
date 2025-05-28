a02afeffd1e4ba3feb960c92c80e7fb5a70b7366
hubRKgit2025 <karim.akmr.monscho06@gmail.com>
2025-05-28 22:53:59 +0600

    Update RDSML-Day-10. SPlot.R (Part-1).R

--- RDSML-Day-10 👉 Scatter Plot in R (Part-1)/RDSML-Day-10. SPlot.R (Part-1).R
+++ RDSML-Day-10 👉 Scatter Plot in R (Part-1)/RDSML-Day-10. SPlot.R (Part-1).R
@@ -1,2 +1,19 @@
 installed.packages("ggplot2")
 library(ggplot2)
+
+datasets = mtcars
+
+# Generating a basic scatter plot
+ggplot(datasets, aes(x = wt, y = mpg)) +
+  geom_point()
+
+# Changing the point size and shape
+ggplot(datasets, aes(x = wt, y = mpg)) +
+  geom_point(size = 1, shape =18)
+
+# Adding the regression line
+ggplot(datasets, aes(x = wt, y = mpg)) +
+  geom_point(size = 1, shape =18)
+  geom_smooth(size = 1, shape =18)
+
+
